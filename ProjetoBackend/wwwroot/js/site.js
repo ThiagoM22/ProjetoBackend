3b33780d8ab03352e1dd745b6ffc54c4e4bd6320
ThiagoMZZ <thiagodev728@gmail.com>
2024-11-05 11:09:45 -0300

    Nav bar e footer

--- ProjetoBackend/wwwroot/js/site.js
+++ ProjetoBackend/wwwroot/js/site.js
@@ -1,1 +1,15 @@
 ﻿
+// Seleciona todos os itens de navegação
+const navLinks = document.querySelectorAll('.navbar-nav .custom-link');
+
+// Obtém o caminho do URL atual
+const currentPath = window.location.pathname;
+
+// Adiciona a classe 'active' ao link que corresponde ao caminho atual e remove dos demais
+navLinks.forEach(link => {
+    if (link.getAttribute('href') === currentPath) {
+        link.classList.add('active');
+    } else {
+        link.classList.remove('active');
+    }
+});
